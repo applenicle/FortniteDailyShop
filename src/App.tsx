ddd16c46c5395fb14929a49361fb214bb4be19ce
Applenicle <gamer31122002@gmail.com>
2022-09-26 15:52:04 +0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import './App.css';
-import Card from './Components/Card';
-import Header from './Components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
-// declare global {
-//   interface Window {
-//     Telegram?: any;
-//   }
-// }
+declare global {
+  interface Window {
+    Telegram?: any;
+  }
+}
 
-// const telegram = window.Telegram.WebApp;
+const telegram = window.Telegram.WebApp;
 
 // console.log(telegram);
 
 const App: React.FC = () => {
   return (
-    <div className="bg">
-      <div className="container">
-        <Header />
-        <Card />
-      </div>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
