782ef00b2ff74fa6d57c73d863e7282a24225514
Applenicle <gamer31122002@gmail.com>
2022-09-26 13:27:02 +0500

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/app.scss';
-import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { store } from './redux/store';
 import App from './App';
+import './scss/app.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
